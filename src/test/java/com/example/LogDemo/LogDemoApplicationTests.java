1b9b637c128afb40bf4d5720c17e3cc2cc8e30b2
mahak <>
2021-10-25 20:03:21 +0530

    Project Setup

---
+++ src/test/java/com/example/LogDemo/LogDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LogDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
